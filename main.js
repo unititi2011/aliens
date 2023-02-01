4c5af2f7e0a9f2bd8c2f37a92e7ee97f7a1431aa
unititi2011 <107140140+unititi2011@users.noreply.github.com>
2023-02-01 14:12:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+//https://teachablemachine.withgoogle.com/models/SK9bUtae5/
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/SK9bUtae5/model.json",modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults (error,results){
+if (error){
+    console.error(error);
+}else{
+    console.log(results);
+    rnr=Math.floor(Math.random()*255)+1;
+    rng=Math.floor(Math.random()*255)+1;
+    rnb=Math.floor(Math.random()*255)+1;
+    document.getElementById("result_label").innerHTML = 'Posso ouvir -'+results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Precisão -'+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color="rgb("+rnr+","+rng+","+rnb+")";
+    document.getElementById("result_confidence").style.color="rgb("+rnr+","+rng+","+rnb+")";
+
+img=document.getElementById("alien1")
+img1=document.getElementById("alien2")
+img2=document.getElementById("alien3")
+img3=document.getElementById("alien4")
+
+if(results[0].label=="palmas"){
+    img.src="aliens-01.gif";
+    img1.src="aliens-02.png";
+    img2.src="aliens-03.png";
+    img3.src="aliens-04.png";
+
+}else if(results[0].label=="sinos"){
+    img.src="aliens-01.png";
+    img1.src="aliens-02.gif";
+    img2.src="aliens-03.png";
+    img3.src="aliens-04.png";
+}
+else if(results[0].label=="estalos"){
+    img.src="aliens-01.png";
+    img1.src="aliens-02.png";
+    img2.src="aliens-03.gif";
+    img3.src="aliens-04.png";
+}
+else{
+    img.src="aliens-01.png";
+    img1.src="aliens-02.png";
+    img2.src="aliens-03.png";
+    img3.src="aliens-04.gif";
+}
+}
+}
